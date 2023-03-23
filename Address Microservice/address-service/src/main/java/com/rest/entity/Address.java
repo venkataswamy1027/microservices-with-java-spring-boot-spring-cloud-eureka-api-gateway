3e572aa38d900ac7358303b1eed8c8867f2066aa
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-03-23 12:37:26 +0530

    Section 6: Address Microservice

---
+++ Address Microservice/address-service/src/main/java/com/rest/entity/Address.java
@@ -0,0 +1,28 @@
+package com.rest.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "address")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "street")
+	private String street;
+
+	@Column(name = "city")
+	private String city;
+
+}
