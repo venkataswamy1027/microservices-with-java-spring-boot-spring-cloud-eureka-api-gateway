843b60ac31f78f7271578d2a2fdf8342eb129e09
IppeVenkataswamy <ippe.venkataswamy@gmail.com>
2023-03-26 10:55:40 +0530

    Section 11: Spring Cloud API Gateway

---
+++ Create-Spring-Cloud-API-Gateway/student-service/src/main/java/com/rest/service/StudentService.java
@@ -0,0 +1,61 @@
+package com.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.rest.entity.Student;
+import com.rest.feignclients.AddressFeignClient;
+import com.rest.repository.StudentRepository;
+import com.rest.request.CreateStudentRequest;
+import com.rest.response.AddressResponse;
+import com.rest.response.StudentResponse;
+
+import reactor.core.publisher.Mono;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	StudentRepository studentRepository;
+	
+	@Autowired
+	WebClient webClient;
+
+	@Autowired
+	AddressFeignClient addressFeignClient;
+	
+	public StudentResponse createStudent(CreateStudentRequest createStudentRequest) {
+
+		Student student = new Student();
+		student.setFirstName(createStudentRequest.getFirstName());
+		student.setLastName(createStudentRequest.getLastName());
+		student.setEmail(createStudentRequest.getEmail());
+		
+		student.setAddressId(createStudentRequest.getAddressId());
+		student = studentRepository.save(student);
+		
+		StudentResponse studentResponse = new StudentResponse(student);
+		
+		//studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
+		studentResponse.setAddressResponse(addressFeignClient.getById(student.getAddressId()));
+		return studentResponse;
+	}
+	
+	public StudentResponse getById (long id) {
+		Student student = studentRepository.findById(id).get();
+		StudentResponse studentResponse = new StudentResponse(student);
+		
+		//studentResponse.setAddressResponse(getAddressById(student.getAddressId()));
+		studentResponse.setAddressResponse(addressFeignClient.getById(student.getAddressId()));
+		return studentResponse;
+	}
+	
+	public AddressResponse getAddressById (long addressId) {
+		Mono<AddressResponse> addressResponse = 
+				webClient.get().uri("/getById/" + addressId)
+		.retrieve().bodyToMono(AddressResponse.class);
+		
+		return addressResponse.block();
+	}
+}
